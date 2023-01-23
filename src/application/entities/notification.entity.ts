fbaf83886cdb74189e6cd8e7eb0a03ee08f4ac6d
Luís Otávio De Simone <85963351+luisotaviodesimone@users.noreply.github.com>
2023-01-23 18:14:10 -0300

    feat: create new repository methods

--- src/application/entities/notification.entity.ts
+++ src/application/entities/notification.entity.ts
@@ -2,7 +2,7 @@ import { randomUUID } from 'crypto';
 import { Replace } from '@helpers/typing.helper';
 import { Content } from './content.entity';
 
-interface NotificationProps {
+export interface NotificationProps {
   recipientId: string;
   content: Content;
   category: string;
--- test/application/repositories/in-memory-notifications.repository.ts
+++ test/application/repositories/in-memory-notifications.repository.ts
@@ -16,10 +16,30 @@ export class InMemoryNotificationsRepository
   }
 
   public async save(notification: Notification): Promise<void> {
-    throw new Error('Method not implemented.');
+    const notificationIndex = this.notifications.findIndex(
+      (item) => item.id === notification.id,
+    );
+
+    if (notificationIndex >= 0) {
+      this.notifications[notificationIndex] = notification;
+    }
   }
 
   public async create(notification: Notification) {
     this.notifications.push(notification);
+  }
+
+  public async countManyByRecipientId(recipientId: string): Promise<number> {
+    return this.notifications.filter(
+      (notification) => notification.recipientId === recipientId,
+    ).length;
+  }
+
+  public async findManyByRecipientId(
+    recipientId: string,
+  ): Promise<Notification[]> {
+    return this.notifications.filter(
+      (notification) => notification.recipientId === recipientId,
+    );
   }
 }
