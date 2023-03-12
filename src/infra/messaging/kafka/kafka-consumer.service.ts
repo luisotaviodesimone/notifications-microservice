9515a0aaf0497368a463046080c9a091ef7b219e
Luís Otávio De Simone <85963351+luisotaviodesimone@users.noreply.github.com>
2023-03-12 18:15:44 -0300

    feat: create kafka consumer service

---
+++ src/infra/messaging/kafka/kafka-consumer.service.ts
@@ -0,0 +1,12 @@
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { ServerKafka } from '@nestjs/microservices';
+
+@Injectable()
+export class KafkaConsumerService
+  extends ServerKafka
+  implements OnModuleDestroy
+{
+  onModuleDestroy() {
+    throw new Error('Method not implemented.');
+  }
+}
