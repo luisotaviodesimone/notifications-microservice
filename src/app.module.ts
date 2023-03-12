f986e8fd78a19c9a42ccca18be03af9bfebef6fb
Luís Otávio De Simone <85963351+luisotaviodesimone@users.noreply.github.com>
2023-03-12 18:00:29 -0300

    feat: add messaging module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { DatabaseModule } from '@infra/database/database.module';
 import { HttpModule } from '@infra/http/http.module';
+import { MessagingModule } from '@infra/messaging/messaging.module';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [HttpModule, DatabaseModule],
+  imports: [HttpModule, DatabaseModule, MessagingModule],
   providers: [],
   controllers: [],
 })
